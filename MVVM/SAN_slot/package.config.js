28e5a24259823fc823048e14c342aa0820e4da22
QingyiHuang <297947067@qq.com>
2018-05-07 22:13:06 +0800

    2.5 need to handle,2.6 is doing and learn san.js by vuegit add MVVM!

---
+++ MVVM/SAN_slot/package.config.js
@@ -0,0 +1,10 @@
+"devDependencies": {
+  "babel-core": "^6.26.3",
+  "babel-loader": "^7.1.4",
+  "css-loader": "^0.28.11",
+  "file-loader": "^1.1.11",
+  "html-loader": "^0.5.5",
+  "san": "^3.5.7",
+  "san-loader": "^0.0.7",
+  "style-loader": "^0.21.0"
+}
